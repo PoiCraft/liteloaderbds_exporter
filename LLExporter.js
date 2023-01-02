21667df8898b1684a85dd927f6e3fc44294d5b6a
gggxbbb <2331490629@qq.com>
2023-01-02 13:48:31 +0800

    Add files via upload

--- LLExporter.js
+++ LLExporter.js
@@ -56,7 +56,7 @@ class Metrics {
 
 }
 
-// Metrics
+//// Metrics
 
 // Players
 function metrics_players_online_count() {
@@ -143,127 +143,351 @@ function metrics_players_device() {
 function metrics_entities_count() {
     const entities = mc.getAllEntities();
 
-    const all = new Metrics("entities_count", entities.length, [new Label("filter", "all")]);
-    const player = new Metrics("entities_count", 0, [new Label("filter", "player")]);
-    const item = new Metrics("entities_count", 0, [new Label("filter", "item")]);
+    const data = {
+        all: {
+            all: entities.length,
+        },
+        player: {
+            all: 0,
+        },
+        item: {
+            all: 0,
+        },
 
-    const can_fly = new Metrics("entities_count", 0, [new Label("filter", "can_fly")]);
-    const can_freeze = new Metrics("entities_count", 0, [new Label("filter", "can_freeze")]);
-    const can_see_daylight = new Metrics("entities_count", 0, [new Label("filter", "can_see_daylight")]);
-    const can_pickup_items = new Metrics("entities_count", 0, [new Label("filter", "can_pickup_items")]);
+        can_fly: {
+            all: 0,
+        },
+        can_freeze: {
+            all: 0,
+        },
+        can_see_daylight: {
+            all: 0,
+        },
+        can_pickup_items: {
+            all: 0,
+        },
 
-    const in_air = new Metrics("entities_count", 0, [new Label("filter", "in_air")]);
-    const in_water = new Metrics("entities_count", 0, [new Label("filter", "in_water")]);
-    const in_lava = new Metrics("entities_count", 0, [new Label("filter", "in_lava")]);
-    const in_rain = new Metrics("entities_count", 0, [new Label("filter", "in_rain")]);
-    const in_snow = new Metrics("entities_count", 0, [new Label("filter", "in_snow")]);
-    const in_wall = new Metrics("entities_count", 0, [new Label("filter", "in_wall")]);
-    const in_water_or_rain = new Metrics("entities_count", 0, [new Label("filter", "in_water_or_rain")]);
-    const in_world = new Metrics("entities_count", 0, [new Label("filter", "in_world")]);
-    const is_invisible = new Metrics("entities_count", 0, [new Label("filter", "is_invisible")]);
-    const is_inside_portal = new Metrics("entities_count", 0, [new Label("filter", "is_inside_portal")]);
-    const is_trusting = new Metrics("entities_count", 0, [new Label("filter", "is_trusting")]);
-    const is_touching_damage_block = new Metrics("entities_count", 0, [new Label("filter", "is_touching_damage_block")]);
-    const is_on_fire = new Metrics("entities_count", 0, [new Label("filter", "is_on_fire")]);
-    const is_on_ground = new Metrics("entities_count", 0, [new Label("filter", "is_on_ground")]);
-    const is_on_hot_block = new Metrics("entities_count", 0, [new Label("filter", "is_on_hot_block")]);
-    const is_trading = new Metrics("entities_count", 0, [new Label("filter", "is_trading")]);
-    const is_riding = new Metrics("entities_count", 0, [new Label("filter", "is_riding")]);
-    const is_dancing = new Metrics("entities_count", 0, [new Label("filter", "is_dancing")]);
-    const is_sleeping = new Metrics("entities_count", 0, [new Label("filter", "is_sleeping")]);
-    const is_angry = new Metrics("entities_count", 0, [new Label("filter", "is_angry")]);
-    const is_baby = new Metrics("entities_count", 0, [new Label("filter", "is_baby")]);
-    const is_moveing = new Metrics("entities_count", 0, [new Label("filter", "is_moveing")]);
+        in_air: {
+            all: 0,
+        },
+        in_water: {
+            all: 0,
+        },
+        in_lava: {
+            all: 0,
+        },
+        in_rain: {
+            all: 0,
+        },
+        in_snow: {
+            all: 0,
+        },
+        in_wall: {
+            all: 0,
+        },
+        in_water_or_rain: {
+            all: 0,
+        },
+        in_world: {
+            all: 0,
+        },
+
+        is_invisible: {
+            all: 0,
+        },
+        is_inside_portal: {
+            all: 0,
+        },
+        is_trusting: {
+            all: 0,
+        },
+        is_touching_damage_block: {
+            all: 0,
+        },
+        is_on_fire: {
+            all: 0,
+        },
+        is_on_ground: {
+            all: 0,
+        },
+        is_on_hot_block: {
+            all: 0,
+        },
+        is_trading: {
+            all: 0,
+        },
+        is_riding: {
+            all: 0,
+        },
+        is_dancing: {
+            all: 0,
+        },
+        is_sleeping: {
+            all: 0,
+        },
+        is_angry: {
+            all: 0,
+        },
+        is_baby: {
+            all: 0,
+        },
+        is_moving: {
+            all: 0,
+        },
+    };
 
     for (const entity of entities) {
+
+        if (data.all.hasOwnProperty(entity.type)) {
+            data.all[entity.type]++;
+        } else {
+            data.all[entity.type]=1;
+        }
+
         if (entity.isPlayer()) {
-            player.value++;
+            data.player.all++;
+            if (data.player.hasOwnProperty(entity.type)) {
+                data.player[entity.type]++;
+            } else {
+                data.player[entity.type]=1;
+            }
         }
         if (entity.isItemEntity()) {
-            item.value++;
+            data.item.all++;
+            if (data.item.hasOwnProperty(entity.type)) {
+                data.item[entity.type]++;
+            } else {
+                data.item[entity.type]=1;
+            }
         }
         if (entity.canFly) {
-            can_fly.value++;
+            data.can_fly.all++;
+            if (data.can_fly.hasOwnProperty(entity.type)) {
+                data.can_fly[entity.type]++;
+            } else {
+                data.can_fly[entity.type]=1;
+            }
         }
         if (entity.canFreeze) {
-            can_freeze.value++;
+            data.can_freeze.all++;
+            if (data.can_freeze.hasOwnProperty(entity.type)) {
+                data.can_freeze[entity.type]++;
+            } else {
+                data.can_freeze[entity.type]=1;
+            }
         }
         if (entity.canSeeDaylight) {
-            can_see_daylight.value++;
+            data.can_see_daylight.all++;
+            if (data.can_see_daylight.hasOwnProperty(entity.type)) {
+                data.can_see_daylight[entity.type]++;
+            } else {
+                data.can_see_daylight[entity.type]=1;
+            }
         }
         if (entity.canPickupItems) {
-            can_pickup_items.value++;
+            data.can_pickup_items.all++;
+            if (data.can_pickup_items.hasOwnProperty(entity.type)) {
+                data.can_pickup_items[entity.type]++;
+            } else {
+                data.can_pickup_items[entity.type]=1;
+            }
         }
         if (entity.inAir) {
-            in_air.value++;
+            data.in_air.all++;
+            if (data.in_air.hasOwnProperty(entity.type)) {
+                data.in_air[entity.type]++;
+            } else {
+                data.in_air[entity.type]=1;
+            }
         }
         if (entity.inWater) {
-            in_water.value++;
+            data.in_water.all++;
+            if (data.in_water.hasOwnProperty(entity.type)) {
+                data.in_water[entity.type]++;
+            } else {
+                data.in_water[entity.type]=1;
+            }
         }
         if (entity.inLava) {
-            in_lava.value++;
+            data.in_lava.all++;
+            if (data.in_lava.hasOwnProperty(entity.type)) {
+                data.in_lava[entity.type]++;
+            } else {
+                data.in_lava[entity.type]=1;
+            }
         }
         if (entity.inRain) {
-            in_rain.value++;
+            data.in_rain.all++;
+            if (data.in_rain.hasOwnProperty(entity.type)) {
+                data.in_rain[entity.type]++;
+            } else {
+                data.in_rain[entity.type]=1;
+            }
         }
         if (entity.inSnow) {
-            in_snow.value++;
+            data.in_snow.all++;
+            if (data.in_snow.hasOwnProperty(entity.type)) {
+                data.in_snow[entity.type]++;
+            } else {
+                data.in_snow[entity.type]=1;
+            }
         }
         if (entity.inWall) {
-            in_wall.value++;
+            data.in_wall.all++;
+            if (data.in_wall.hasOwnProperty(entity.type)) {
+                data.in_wall[entity.type]++;
+            } else {
+                data.in_wall[entity.type]=1;
+            }
         }
         if (entity.inWaterOrRain) {
-            in_water_or_rain.value++;
+            data.in_water_or_rain.all++;
+            if (data.in_water_or_rain.hasOwnProperty(entity.type)) {
+                data.in_water_or_rain[entity.type]++;
+            } else {
+                data.in_water_or_rain[entity.type]=1;
+            }
         }
         if (entity.inWorld) {
-            in_world.value++;
+            data.in_world.all++;
+            if (data.in_world.hasOwnProperty(entity.type)) {
+                data.in_world[entity.type]++;
+            } else {
+                data.in_world[entity.type]=1;
+            }
         }
         if (entity.isInvisible) {
-            is_invisible.value++;
+            data.is_invisible.all++;
+            if (data.is_invisible.hasOwnProperty(entity.type)) {
+                data.is_invisible[entity.type]++;
+            } else {
+                data.is_invisible[entity.type]=1;
+            }
         }
         if (entity.isInsidePortal) {
-            is_inside_portal.value++;
+            data.is_inside_portal.all++;
+            if (data.is_inside_portal.hasOwnProperty(entity.type)) {
+                data.is_inside_portal[entity.type]++;
+            } else {
+                data.is_inside_portal[entity.type]=1;
+            }
         }
         if (entity.isTrusting) {
-            is_trusting.value++;
+            data.is_trusting.all++;
+            if (data.is_trusting.hasOwnProperty(entity.type)) {
+                data.is_trusting[entity.type]++;
+            } else {
+                data.is_trusting[entity.type]=1;
+            }
         }
         if (entity.isTouchingDamageBlock) {
-            is_touching_damage_block.value++;
+            data.is_touching_damage_block.all++;
+            if (data.is_touching_damage_block.hasOwnProperty(entity.type)) {
+                data.is_touching_damage_block[entity.type]++;
+            } else {
+                data.is_touching_damage_block[entity.type]=1;
+            }
         }
         if (entity.isOnFire) {
-            is_on_fire.value++;
+            data.is_on_fire.all++;
+            if (data.is_on_fire.hasOwnProperty(entity.type)) {
+                data.is_on_fire[entity.type]++;
+            } else {
+                data.is_on_fire[entity.type]=1;
+            }
         }
         if (entity.isOnGround) {
-            is_on_ground.value++;
+            data.is_on_ground.all++;
+            if (data.is_on_ground.hasOwnProperty(entity.type)) {
+                data.is_on_ground[entity.type]++;
+            } else {
+                data.is_on_ground[entity.type]=1;
+            }
         }
         if (entity.isOnHotBlock) {
-            is_on_hot_block.value++;
+            data.is_on_hot_block.all++;
+            if (data.is_on_hot_block.hasOwnProperty(entity.type)) {
+                data.is_on_hot_block[entity.type]++;
+            } else {
+                data.is_on_hot_block[entity.type]=1;
+            }
         }
         if (entity.isTrading) {
-            is_trading.value++;
+            data.is_trading.all++;
+            if (data.is_trading.hasOwnProperty(entity.type)) {
+                data.is_trading[entity.type]++;
+            } else {
+                data.is_trading[entity.type]=1;
+            }
         }
         if (entity.isRiding) {
-            is_riding.value++;
+            data.is_riding.all++;
+            if (data.is_riding.hasOwnProperty(entity.type)) {
+                data.is_riding[entity.type]++;
+            } else {
+                data.is_riding[entity.type]=1;
+            }
         }
         if (entity.isDancing) {
-            is_dancing.value++;
+            data.is_dancing.all++;
+            if (data.is_dancing.hasOwnProperty(entity.type)) {
+                data.is_dancing[entity.type]++;
+            } else {
+                data.is_dancing[entity.type]=1;
+            }
         }
         if (entity.isSleeping) {
-            is_sleeping.value++;
+            data.is_sleeping.all++;
+            if (data.is_sleeping.hasOwnProperty(entity.type)) {
+                data.is_sleeping[entity.type]++;
+            } else {
+                data.is_sleeping[entity.type]=1;
+            }
         }
         if (entity.isAngry) {
-            is_angry.value++;
+            data.is_angry.all++;
+            if (data.is_angry.hasOwnProperty(entity.type)) {
+                data.is_angry[entity.type]++;
+            } else {
+                data.is_angry[entity.type]=1;
+            }
         }
         if (entity.isBaby) {
-            is_baby.value++;
+            data.is_baby.all++;
+            if (data.is_baby.hasOwnProperty(entity.type)) {
+                data.is_baby[entity.type]++;
+            } else {
+                data.is_baby[entity.type]=1;
+            }
         }
         if (entity.isMoving) {
-            is_moveing.value++;
+            data.is_moving.all++;
+            if (data.is_moving.hasOwnProperty(entity.type)) {
+                data.is_moving[entity.type]++;
+            } else {
+                data.is_moving[entity.type]=1;
+            }
         }
 
     }
 
-    return [all, player, item, can_fly, can_freeze, can_see_daylight, can_pickup_items, in_air, in_water, in_lava, in_rain, in_snow, in_wall, in_water_or_rain, in_world, is_invisible, is_inside_portal, is_trusting, is_touching_damage_block, is_on_fire, is_on_ground, is_on_hot_block, is_trading, is_riding, is_dancing, is_sleeping, is_angry, is_baby, is_moveing];
+    const to_return = [];
+    for (const [filter, value] of Object.entries(data)) {
+        for (const [type, value2] of Object.entries(value)) {
+            to_return.push(
+                new Metrics(
+                    "entities_count",
+                    value2,
+                    [
+                        new Label("filter", filter),
+                        new Label("type", type),
+                    ]
+                )
+            );
+        }
+    }
+
+    return to_return;
 }
 
 // Performance
@@ -275,13 +499,16 @@ function metrics_performance() {
     return [mspt_metric, tps_metric];
 }
 
-// Metrics End
+//// Metrics End
 
-function get_mspt_from_trapdoor(){
+
+//// Utils
+
+function get_mspt_from_trapdoor() {
     const result = mc.runcmdEx("log mspt");
-    if (result.success){
+    if (result.success) {
         const reg = new RegExp("§.");
-        const opt = result.output.replace(reg,"").split("\n")[0].split(":")[1];
+        const opt = result.output.replace(reg, "").split("\n")[0].split(":")[1];
         const nums = opt.split("/");
         const mspt = parseFloat(nums[0]);
         const tps = parseFloat(nums[1]);
@@ -289,5 +516,7 @@ function get_mspt_from_trapdoor(){
     }
     return [NaN, NaN];
 }
+
+//// Utils End
 
 server.startAt("127.0.0.1", 9999);
